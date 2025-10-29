1ca759dbad69fc5ecc1d9fdbcb2daf38c3c3e490
handywashiskar <handyray676@gmail.com>
2025-10-29 12:27:27 +0300

    Delete modules/youtube.js

--- modules/youtube.js
+++
@@ -1,9 +0,0 @@
-export function render(app) {
-  app.innerHTML = `
-    <section class="youtube">
-      <h2>📺 My YouTube</h2>
-      <p>View and manage your YouTube channel content.</p>
-      <div class="youtube-preview">[Embedded videos here]</div>
-    </section>
-  `;
-}
