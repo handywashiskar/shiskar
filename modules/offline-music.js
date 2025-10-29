bc8380e6f2dc567d0b47d7124de2a18c225327b1
handywashiskar <handyray676@gmail.com>
2025-10-29 12:26:26 +0300

    Delete modules/offline-music.js

--- modules/offline-music.js
+++
@@ -1,9 +0,0 @@
-export function render(app) {
-  app.innerHTML = `
-    <section class="offline-music">
-      <h2>📁 Offline Music</h2>
-      <p>Manage and play your offline tracks anytime.</p>
-      <div class="offline-list">[Offline tracks will appear here]</div>
-    </section>
-  `;
-}
