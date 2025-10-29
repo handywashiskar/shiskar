9688ee2380497c6d318395cfb2583ebda004a0aa
handywashiskar <handyray676@gmail.com>
2025-10-29 13:00:47 +0300

    Create intro.js

---
+++ modules/intro.js
@@ -0,0 +1,8 @@
+export function render(app) {
+  app.innerHTML = `
+    <section class="intro">
+      <h2>👋 Welcome to Shiskar Studio</h2>
+      <p>Your creative command center is ready.</p>
+    </section>
+  `;
+}
