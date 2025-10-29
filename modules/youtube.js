fb80941ab52d4a98a2270d8d28075fd8f80d2c85
handywashiskar <handyray676@gmail.com>
2025-10-29 11:31:13 +0300

    Create youtube.js

---
+++ modules/youtube.js
@@ -0,0 +1,9 @@
+export function render(app) {
+  app.innerHTML = `
+    <section class="youtube">
+      <h2>📺 My YouTube</h2>
+      <p>View and manage your YouTube channel content.</p>
+      <div class="youtube-preview">[Embedded videos here]</div>
+    </section>
+  `;
+}
