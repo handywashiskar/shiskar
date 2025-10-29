ab8b9055fc106ca7ce0caf41e253101bb1b71f34
handywashiskar <handyray676@gmail.com>
2025-10-29 11:29:46 +0300

    Create offline-music.js

---
+++ modules/offline-music.js
@@ -0,0 +1,9 @@
+export function render(app) {
+  app.innerHTML = `
+    <section class="offline-music">
+      <h2>📁 Offline Music</h2>
+      <p>Manage and play your offline tracks anytime.</p>
+      <div class="offline-list">[Offline tracks will appear here]</div>
+    </section>
+  `;
+}
